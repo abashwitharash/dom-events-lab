b35adf775ed01ebec36d6478e3643e027da3ee69
Arash <arashsaleh798@gmail.com>
2025-01-24 21:40:04 -0800

    step 2 complete

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
 const buttons = document.querySelectorAll('.button');
 
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+     
+      console.log(event.target.innerText);
+ 
+    });
+  });
+
 
 
   
