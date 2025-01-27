a0878402ca02128f33e540bfee333ced2e128c1a
Arash <arashsaleh798@gmail.com>
2025-01-26 17:58:16 -0800

    added variables

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const buttons = document.querySelectorAll('.button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-     
-      console.log(event.target.innerText);
- 
-    });
-  });
+        console.log(event.target.innerText);
+    }); 
+});
 
-  const calculator = document.querySelector('#calculator');
+const calculator = document.querySelector('#calculator')
 
-  
+
+
 
 /*-------------------------------- Variables --------------------------------*/
-
+let firstValue = '';
+let secondValue = '';
+let operation = '';
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
